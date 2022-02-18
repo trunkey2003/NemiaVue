0ba88d343b866bb9f37f680266e7812b74688183
trunkey2007 <hoquanglam090@gmail.com>
2022-02-18 23:22:27 +0700

    getting started

--- components/Home.vue
+++ components/Home.vue
@@ -1,29 +1,113 @@
 <template>
   <div class="app">
-      <div class="container py-6 flex justify-center items-center custom-background">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-6 h-6 mr-2 text-yellow-600"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-        <input
-          type="text"
-          name="name"
-          placeholder="name"
-          class="w-1/2 py-2 border-b-2 border-yellow-400 outline-none focus:border-green-400"
+    <div
+      class="container py-6 flex justify-center items-center custom-background"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-6 h-6 mr-2 text-yellow-600"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
+      </svg>
+      <input
+        type="text"
+        name="anime"
+        placeholder="Search Your First Favourite Anime..."
+        class="w-1/2 py-2 border-b-2 border-yellow-400 outline-none focus:border-green-400"
+      />
+    </div>
+    <div class="container w-full min-h-screen bg-gray-100 rounded-t-2xl">
+      <div class="flex flex-wrap p-10">
+        <!--Card 1-->
+        <div class="max-w-[25%] rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/mountain.jpg" alt="Mountain" />
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Mountain</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#photography</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#travel</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#winter</span
+            >
+          </div>
+        </div>
+
+        <!--Card 1-->
+        <div class="max-w-[25%] rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/mountain.jpg" alt="Mountain" />
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Mountain</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#photography</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#travel</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#winter</span
+            >
+          </div>
+        </div>
+
+        <!--Card 1-->
+        <div class="max-w-[25%] rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/mountain.jpg" alt="Mountain" />
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Mountain</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#photography</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#travel</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >#winter</span
+            >
+          </div>
         </div>
       </div>
-      <div>Hello</div>
+    </div>
   </div>
 </template>
 
