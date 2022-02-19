f3394d759c2712e9c6b33448cf68e9e8f61d302a
trunkey2007 <hoquanglam090@gmail.com>
2022-02-20 00:33:42 +0700

    completed left pagination

--- components/Home.vue
+++ components/Home.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="app">
-    <div
-      class="container py-6 flex justify-center items-center custom-background"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="w-6 h-6 mr-2 text-yellow-600"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
-      <input
-        type="text"
-        name="anime"
-        placeholder="Search Your First Favourite Anime..."
-        class="w-1/2 py-2 border-b-2 border-yellow-400 outline-none focus:border-green-400"
-      />
-    </div>
+    <SearchInput/>
+    <MediaFilter/>
     <div class="container w-full min-h-screen bg-gray-100 rounded-t-2xl">
       <div class="flex flex-wrap p-10">
         <NuxtLink
@@ -54,11 +32,12 @@
             <span
               v-for="genre in media.genres"
               class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-              >#{{genre}}</span
+              >#{{ genre }}</span
             >
           </div>
         </NuxtLink>
       </div>
+      <PageNav/>
     </div>
   </div>
 </template>
@@ -67,8 +46,8 @@
 import gql from "graphql-tag";
 
 const query = gql`
-  query {
-    Page(page: 1, perPage: 20) {
+  query Page($page: Int){
+    Page(page: $page, perPage: 20) {
       media {
         id
         title {
@@ -89,6 +68,11 @@ export default {
     Page: {
       query: query,
       prefetch: true,
+      variables() {
+        return {
+          page: (this.$route.params.page)? this.$route.params.page : 1,
+        };
+      },
     },
   },
 };
